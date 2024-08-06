2a1ea8fd2648de1b2f3022f649e2b7dc04941f9e
TANNU <161803939+TamannaNag@users.noreply.github.com>
2024-08-06 11:48:46 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,83 @@
+// const input = require("readline-sync");
+// let n = input.questionInt("enter the langth of array:");
+// arr = [];
+
+// for(let i = 0 ; i<n; i++)
+// {
+//     arr[i] = input.questionInt("enter the element:");
+// }
+// for(let j = 0; j<n; j++)
+// {
+// console.log(arr[j]);
+// }
+
+
+//-----------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+/*
+When I have the array [1,2,3,4,5,6] as input, I want to print only 2 in 6 times the output.*/
+
+
+
+// const input = require("readline-sync");
+// let n = input.questionInt("enter the langth of array:");
+// arr = [];
+// for(let i = 0; i<n; i++)
+// {
+//     arr[i] = input.questionInt("enter the element");
+// }
+
+// for(let i =  0; i<n; i++)
+// {
+//     console.log(arr[0]);
+// }
+
+
+
+//---------------------------------------------------------------------------------------------------------------
+
+// const input = require("readline-sync");
+
+// let n = input.questionInt("Enter the number of elements: ");
+// let arr = [];
+// let c = 0;
+// let t = input.questionInt("enter the target no.:");
+// for (let i = 0; i < n; i++) 
+// {
+//        arr[i] = input.questionInt("enter the element no. : ")
+
+//        if(arr[i]==t)
+//  {
+//         c++;
+//         i++;
+//         console.log("yes");
+//  } else
+//  {
+//          console.log("no");
+//  }
+// }
+
+//------------------------------------------------------------------------------------------------------------------------------------
+ 
+// length = 5;
+// let arr = [5,10,15,20,25];
+// let i = 0;
+// while(i<length)
+// {
+//   if(i%2 == 0)
+//   {
+//     arr[i] = arr[i]*2;
+//   }
+//   else{
+//     arr[i] = arr[i]/2;
+//   }
+//   i++;
+// }
+// console.log(arr[2]);
+
+
+//----------------------------------------------------------------------------------------------------------------------
